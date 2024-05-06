b2f84513d234982802d4c87d246e5c85f07c2d88
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-06 17:19:48 +0100

    changes

--- src/company/Nav.jsx
+++ src/company/Nav.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import logo from '../assets/company/logo.png'
 
 const Nav = () => {
   return (
     <nav className='w-[87rem] h-[5rem] bg-[#2B2222]  flex items-center justify-center  ml-[1.5rem] absolute top-5 rounded-[2rem]'>
     <div className="flex items-center justify-center">
+      <img src={logo}
+      className='w-[35px]'
+      alt='logo'/>
       
+
     </div>
 
       
