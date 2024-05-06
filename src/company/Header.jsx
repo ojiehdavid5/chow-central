a16edb5f76000629a06da7c2c9fb81372bc23e13
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-07 00:30:53 +0100

    changes

--- src/company/Header.jsx
+++ src/company/Header.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import demand from '../assets/company/demand.svg'
+import spin from '../assets/company/spin.svg'
+import suya from "../assets/company/suya.png"
+import chicken from "../assets/company/chicken.png"
+import rice from '../assets/company/rice.png'
 
 const Header = () => {
   return (
-    <div className='min-h-screen'>
-      NNN
+    <div className='min-h-screen mt-[7rem]'>
+      <div className="flex flex-row min-w-screen h-full space-x-9">
+        <div className="flex flex-col  bg-[#2B2222] w-[40rem] h-[45rem]">
+
+          <div className="flex space-x-0 ml-9">
+          <img src={spin} 
+          className='animate-spin ml-6'
+          alt="" />
+
+            <img src={demand} className='mr-6'  alt="" />
+          </div>
+          <div className="">
+            <h1>About Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In natus enim repellat. Officia maxime autem ipsam sint impedit reiciendis architecto fugit repellendus quia? Harum nostrum libero esse tempora? Dolorum ipsa assumenda velit autem quibusdam voluptatem obcaecati aperiam earum temporibus fugiat.</p>
+          </div>
+
+
+        </div>
+        <div className=" flex">
+          <div className="flex flex-col">
+            <img src={suya}  className='w-[30rem]' alt="" />
+            <img src={chicken} className='w-[30rem]' alt="" />
+          </div>
+
+<img src={rice}  className='w-[30rem]' alt="" />
+        </div>
+
+
+      </div>
+
     </div>
   )
 }
--- src/company/Nav.jsx
+++ src/company/Nav.jsx
@@ -3,20 +3,20 @@ import logo from '../assets/company/logo.png'
 
 const Nav = () => {
   return (
-    <nav className='w-[87rem] h-[5rem] bg-[#2B2222]  flex items-center justify-center  ml-[1.5rem] absolute top-5 rounded-[2rem]  '>
+    <nav className='w-[87rem] h-[5rem] bg-[#2B2222]  flex items-center justify-center  -ml-[2.5rem] absolute top-5 rounded-[2rem]  '>
     <div className="flex items-center justify-between w-full">
       <img src={logo}
-      className='w-[35px]'
+      className='w-[35px] ml-[2rem] bg-[#2B2222] rounded-full'
       alt='logo'/>
 
       <div className=" flex items-center justify-center space-x-12">
-        <p> Company</p>
-        <p> Brand</p>
+        <p className='text-white text-md'> Company</p>
+        <p className='text-white text-md'> Brand</p>
       </div>
       <div className=" flex items-center justify-center space-x-9 mr-[4rem]">
         <p className='text-white text-md'> Contact us</p>
-        <p> Faqs</p>
-        <p> Blogs</p>
+        <p className='text-white text-md'> Faqs</p>
+        <p className='text-white text-md'> Blogs</p>
       </div>
 
 
