9bf98329b1dcb87d856d0dcc37f7a3e5749da030
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-08 01:55:11 +0100

    almost done

--- src/company/Video.jsx
+++ src/company/Video.jsx
@@ -4,28 +4,28 @@ import order from '../assets/company/order.svg'
 
 const Video = () => {
   return (
-    <div className='w-full min-h-screen flex flex-row bg-red-600'>
+    <div className='w-full min-h-screen flex flex-row bg-red-600 rounded-xl '>
         <div className=" w-[60rem]">
-        <video autoPlay loop muted  className='w-[60rem]  m-9 rounded-md   mt-[10rem]'>
-                <source src={chow}  className='h-[70rem] w-[50rem]'  type="video/mp4"/>
+        <video autoPlay loop muted  className='w-full  h-full object-cover rounded-lg  mr-[rem]   p-4'>
+                <source src={chow}  className='h-[200rem] w-[40rem]'  type="video/mp4"/>
                 Your browser doesnot support this tag
 
 
             </video>
 
         </div>
-        <div className="flex flex-col w-[30rem]">
+         <div className="flex flex-col ">
             <img src={order}  className='w-[20rem] flex justify-center  ml-[4rem]'alt=''/>
-            <div className="flex flex-col ml-[3rem]">
+            <div className="flex flex-col ml-[3rem] mt-[9rem ]">
 <h1 className='text-white text-5xl'>Our Mission </h1>
-<p className='text-left mt-9  text-white leading-8 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas delectus ab, cum mollitia perferendis suscipit? Commodi excepturi magnam magni accusamus facilis! Amet delectus facilis aliquam, animi reiciendis perferendis quia vel, a atque, reprehenderit molestiae recusandae nulla facere fugit nostrum vero?</p>
+<p className='text-left mt-9  text-white leading-8 text-xl w-[25rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas delectus ab, cum mollitia perferendis suscipit? Commodi excepturi magnam magni accusamus facilis! Amet delectus facilis aliquam, animi reiciendis perferendis quia vel, a atque, reprehenderit molestiae recusandae nulla facere fugit nostrum vero?</p>
 
             </div>
 
 
         </div>
         
-      
+       
     </div>
   )
 }
