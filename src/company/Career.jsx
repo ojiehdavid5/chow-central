31ca7a223b051c0e9a33d81c20af582ab48b935f
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-08 01:54:21 +0100

    changes

---
+++ src/company/Career.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Career = () => {
+  return (
+    <div className='w-full bg-[#EDE5DC] h-[20rem] mt-8 rounded-xl flex items-center justify-center flex-col space-y-[2rem]'>
+      <h1 className='text-[5rem] text-red-600'>Careers</h1>
+      <button className='w-[15rem] h-[4rem] rounded-md text-white bg-black '> See Open positions</button>
+    </div>
+  )
+}
+
+export default Career
